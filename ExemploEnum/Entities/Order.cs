14474ff9858e23997012cf00db40be1344487cad
Marcelo Corrêa <marcelocorrea.0827@outlook.com>
2021-01-06 19:43:57 -0300

    Alteração no enum OrderStatus

--- ExemploEnum/Entities/Order.cs
+++ ExemploEnum/Entities/Order.cs
@@ -1,8 +1,12 @@
-﻿
+﻿using ExemploEnum.Entities.Enuns;
+using System;
+
 namespace ExemploEnum.Entities
 {
     class Order
     {
-
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
     }
 }
