e9ade9e2ee53be26ad36e068e9b31d320689a1ec
Marcelo Corrêa <marcelocorrea.0827@outlook.com>
2021-01-06 19:47:39 -0300

    Override ToString

--- ExemploEnum/Entities/Order.cs
+++ ExemploEnum/Entities/Order.cs
@@ -8,5 +8,14 @@ namespace ExemploEnum.Entities
         public int Id { get; set; }
         public DateTime Moment { get; set; }
         public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Moment
+                + ", "
+                + Status;
+        }
     }
 }
